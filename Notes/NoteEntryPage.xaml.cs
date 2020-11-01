1390af04b09594b2775b92a9d1af5930afa66295
Dominik Marsalek <marsalekdominik01@gmail.com>
2020-11-01 23:08:53 +0100

    Vše důležité

---
+++ Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Notes.Models;
+
+namespace Notes
+{
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+    }
+}
